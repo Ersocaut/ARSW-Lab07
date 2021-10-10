5e51fa0d4dbb341b745bc780b1bbcf510086b137
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2021-10-09 22:47:30 -0500

    PARTE FINAL

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,5 +1,5 @@
 var app = (function () {
-    
+
     class Point{
         constructor(x,y){
             this.x=x;
@@ -12,17 +12,17 @@ var app = (function () {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     var input = document.querySelector('#idDrawing');
-    
+
     var loadEventListener = function(){
         if( input ) input.addEventListener('change', updateId);
         const eventCanvas = (window.PointerEvent)?'pointerdown':'mousedown';
         canvas.addEventListener(eventCanvas, eventPoint);
     }
-
+    
     var updateId = function(event){
         idDrawing = event.target.value;
         console.log(`nuevoValor ${idDrawing}`);
-        if( stompClient ) stompClient.disconnect();
+        app.disconnect();
     }
 
     var eventPoint = function (event){
@@ -31,11 +31,25 @@ var app = (function () {
         if(idDrawing) app.publishPoint(x,y);
     }
 
-    var addPointToCanvas = function (point) {        
+    var addPointToCanvas = function (point) {
         ctx.beginPath();
         ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
         ctx.stroke();
     };
+    
+    var drawPolygon = function(points){
+        canvas.width = canvas.width;
+        var { x: fstPosX, y: fstPosY } = points[0];
+        ctx.moveTo(fstPosX, fstPosY);
+        
+        points.forEach(point => {
+            const { x , y } = point;
+            ctx.arc(x, y, 3, 0, 2 * Math.PI);
+            ctx.lineTo(x,y);
+        });
+        ctx.lineTo(fstPosX, fstPosY);
+        ctx.stroke();
+    }
     
     var getMousePosition = function (evt) {
         var rect = canvas.getBoundingClientRect();
@@ -51,16 +65,32 @@ var app = (function () {
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
         
-        //subscribe to /topic/TOPICXX when connections succeed
+        // Esto es para escuchar un evento cuando llega un nuevo mensaje desde el servidor
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe(`/topic/newpoint.${idDrawing}`, function (eventbody) {
-                var obj = JSON.parse(eventbody.body);
-                addPointToCanvas(obj);
-            });
-        });
-    };    
 
+            //Limpiamos la pantalla
+            canvas.width = canvas.width;
+            stompClient.subscribe(`/topic/newpoint.${idDrawing}`, function (eventbody) {
+                const point = JSON.parse(eventbody.body);
+                addPointToCanvas(point);
+            });
+
+            stompClient.subscribe(`/topic/newpolygon.${idDrawing}`, function (eventbody) {
+                const points = JSON.parse(eventbody.body);
+                drawPolygon(points);
+            });
+            stompClient.subscribe(`/topic/queue.${idDrawing}`, function (eventbody) {
+                const points = JSON.parse(eventbody.body);
+                if( points.length  < 4 ){
+                    points.forEach(point => addPointToCanvas(point));
+                    return;
+                }
+                drawPolygon(points);
+            });
+            stompClient.send(`/app/queue.${idDrawing}`, {});
+        });
+    };
     
     return {
 
@@ -72,15 +102,14 @@ var app = (function () {
             var pt=new Point(px,py);
             console.info("publishing point at "+pt);
             addPointToCanvas(pt);
-            //publicar el evento
-            stompClient.send(`/topic/newpoint.${idDrawing}`, {}, JSON.stringify(pt));
+            stompClient.send(`/app/newpoint.${idDrawing}`, {}, JSON.stringify(pt));
         },
 
         disconnect: function () {
             if (stompClient !== null) {
                 stompClient.disconnect();
             }
-            setConnected(false);
+            /* setConnected(false); */
             console.log("Disconnected");
         },
         publishDrawing( currentId ){
@@ -91,6 +120,9 @@ var app = (function () {
     };
 
 })();
+
+
+
 
 
 //otra forma para publicar un evento -> , enviar puntos al broker (servidor Spring) y mostrara el cambio a los demans clientes
